c28e89ad13e00136b4d26740e38f44b081daf2c9
thedmd <michcic@gmail.com>
2013-03-07 22:32:11 +0000

    Add missing file.

---
+++ IPATools/Utilities/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace IPATools.Utilities
+{
+    public class Utils
+    {
+        public static void CopyStream(Stream from, Stream to)
+        {
+            CopyStream(from, to, 4096);
+        }
+
+        public static void CopyStream(Stream from, Stream to, int bufferSize)
+        {
+            byte[] buffer = new byte[bufferSize];
+            int count;
+            while ((count = from.Read(buffer, 0, buffer.Length)) != 0)
+                to.Write(buffer, 0, count);
+        }
+    }
+}
